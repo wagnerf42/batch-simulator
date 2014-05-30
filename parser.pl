c8ece7f991a4c8bb29d589e943f7765623ba6307
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2014-05-30 12:21:42 +0200

    Mofications made by Frederic
    Also renamed the test file to parser.pl.

--- parser.pl
+++ parser.pl
@@ -1,39 +1,33 @@
 #!/usr/bin/perl
 
+
+use strict;
+use warnings;
+
 my @trace_data;
 my $job = 0;
 
 # Prints one status field line
 sub print_status_field {
 	my @fields = @_;
-
-	print "$fields[1]";
-
-	foreach $field (2 .. scalar @fields) {
-		print " $fields[$field]";
-	}
-
-	print "\n";
+	shift @fields;
+	print "@fields\n";
 }
 
 open (FILE, $ARGV[0]);
 
-while (<FILE>) {
-	@fields = split(" ");
+while (my $line = <FILE>) {
+	my @fields = split(' ', $line);
 
+	next unless defined $fields[0];
 	# Status line
-	if ($fields[0] == ";") { 
-		#print_status_field(@fields);
+	if ($fields[0] eq ';') { 
+		print_status_field(@fields);
 	}
 
 	# Job line
-	if ($fields[0] != " ") {
-		#print "Job: @fields\n";
-		foreach $field (0 .. 17) {
-			$trace_data[$job][$field] = $fields[$field];
-		}
-
-		$job++;
+	if ($fields[0] ne ' ') {
+		push @trace_data, [@fields];
 	}
 
 }
