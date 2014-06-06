3c43dfc4bb44f5ae4842684e915729d01b7e94b3
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2014-06-06 14:19:22 +0200

    Finished a small version that counts the partitions
    This version counts the number of partitions and then the number of jobs
    for each partition.

--- parser.pl
+++ parser.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 
-
 use strict;
 use warnings;
 
 my @trace_data;
 my $job = 0;
+my $partitions_count = 0;
+my @partitions;
 
 # Prints one status field line
 sub print_status_field {
@@ -20,23 +21,38 @@ while (my $line = <FILE>) {
 	my @fields = split(' ', $line);
 
 	next unless defined $fields[0];
+	
 	# Status line
 	if ($fields[0] eq ';') { 
-		print_status_field(@fields);
+		if ($fields[1] eq 'Partition:') {
+			$partitions_count++;
+		}
+
 	}
 
 	# Job line
-	if ($fields[0] ne ' ') {
+	elsif ($fields[0] ne ' ') {
 		push @trace_data, [@fields];
 	}
 
 }
 
 print "Total jobs: " . scalar @trace_data . "\n";
+print 'Total partitions: ' . $partitions_count . "\n";
 
-#foreach $job (0 .. scalar @trace_data -1) {
-#	print "Job Number: $trace_data[$job][0]\n";
-#}
+for (my $i = 0; $i < $partitions_count; $i++) {
+	$partitions[$i] = 0;
+}
+
+for (my $i = 0; $i < scalar @trace_data; $i++) {
+	$partitions[$trace_data[$i][15] - 1]++;
+}
+
+for (my $i = 0; $i < $partitions_count; $i++) {
+	next unless ($partitions[$i] > 0);
+
+	print 'Partition ' . ($i + 1) . ': ' . $partitions[$i] . "\n";
+}
 
 close (FILE);
 exit;
