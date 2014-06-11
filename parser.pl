50ae6fcd33ed99d3443b1f49f1793d936cd5b27e
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2014-06-11 18:05:01 +0200

    Included some more counting about status and finishing time
    I included in the file some code to analyse the real and declared time
    and the differente status supported in this case, 0, 1 and 5.

--- parser.pl
+++ parser.pl
@@ -3,6 +3,28 @@
 use strict;
 use warnings;
 
+use constant JOB_JUMBER => 0;
+use constant SUBMIT_TIME => 1;
+use constant WAIT_TIME => 2;
+use constant RUN_TIME => 3;
+use constant ALLOCATED_CPUS => 4;
+use constant AVG_CPU_TIME => 5;
+use constant USED_MEMORY => 6;
+use constant REQUESTED_CPUS => 7;
+use constant REQUESTED_TIME => 8;
+use constant REQUESTED_MEMORY => 9;
+use constant STATUS => 10;
+use constant UID => 11;
+use constant GID => 12;
+use constant EXEC_NUMBER => 13;
+use constant QUEUE_NUMBER => 14;
+use constant PARTITION_NUMBER => 15;
+use constant PREC_JOB_JUMBER => 16;
+use constant THINK_TIME_PREC_JOB => 17;
+use constant STATUS_COMPLETED => 1;
+use constant STATUS_FAILED => 0;
+use constant STATUS_CANCELLED => 5;
+
 my @trace_data;
 my @status_data;
 my $job = 0;
@@ -66,14 +88,53 @@ for ($partition_index = 0; $status_index < @status_data and defined $status_data
 # Counting how many times each partition is used. The partitions in the SWF 
 # file start from 1 so I shift them one position
 for (my $i = 0; $i < scalar @trace_data; $i++) {
-	$partitions[$trace_data[$i][15] - 1]++;
+	$partitions[$trace_data[$i][PARTITION_NUMBER] - 1]++;
 }
 
 for (my $i = 0; $i < $partitions_count; $i++) {
 	next unless ($partitions[$i] > 0);
-	print 'Partition ' . ($i + 1) . ': ' . $partition_names[$i] . ': ' . $partitions[$i] . "\n";
+	print 'Partition ' . ($i + 1) . ': ' . $partition_names[$i] . ': ' . $partitions[$i] . ' job(s)' . "\n";
 }
 
+# Counting the number of jobs that finishe before, exactly at or after the
+# requested time
+my $j = 0, my $k = 0, my $l = 0;
+for (my $i = 0; $i < @trace_data; $i++) {
+	if ($trace_data[$i][RUN_TIME] < $trace_data[$i][REQUESTED_TIME]) {
+		$j++;
+	}
+
+	elsif ($trace_data[$i][RUN_TIME] == $trace_data[$i][REQUESTED_TIME]) {
+		$k++;
+	}
+
+	else {
+		$l++;
+	}
+}
+
+print 'Jobs executed less then requested time: ' . "$j\n";
+print 'Jobs executed exactly the requested time: ' . "$k\n";
+print 'Jobs executed more then requested time: ' . "$l\n";
+
+# Counting the number of jobs that finished with the different status
+for (my $i = 0, $j = 0, $k = 0, $l = 0; $i < @trace_data; $i++) {
+	if ($trace_data[$i][STATUS] == STATUS_COMPLETED) {
+		$j++;
+	}
+
+	elsif ($trace_data[$i][STATUS] == STATUS_FAILED) {
+		$k++;
+	}
+
+	else {
+		$l++;
+	}
+}
+	
+print 'Jobs finished with status = COMPLETED: ' . "$j\n";
+print 'Jobs finished with status = FAILED: ' . "$k\n";
+print 'Jobs finished with status = CANCELLED: ' . "$l\n";
 
 
 close (FILE);
