cc1a4dbf9676c217b0910871edfb449958a1ddaf
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2014-07-03 18:18:41 +0200

    Just tested the backfilling and the FCFS algorithms with random traces
    I was testing executing both algorithms with some random traces and it
    is working. The next step is to use the actual trace from Currie and try
    creating some experiments.

--- parser2.pl
+++ parser2.pl
@@ -13,8 +13,18 @@ print "Executing parser version 2\n";
 my $trace = Trace->new($ARGV[0]);
 $trace->read();
 
-my $schedule_backfilling = Backfilling->new($trace, $trace->needed_cpus);
+my $trace_random = Trace->new();
+$trace_random->read_from_trace($trace, 120);
+$trace_random->write("random.swf");
+
+my $schedule_backfilling = Backfilling->new($trace_random, $trace_random->needed_cpus);
 $schedule_backfilling->run();
 $schedule_backfilling->print_svg("parser2.svg", "parser2.pdf");
+
+my $schedule_FCFS = new FCFS($trace_random, $trace_random->needed_cpus);
+$schedule_FCFS->run();
+$schedule_FCFS->print_svg("parser2_fcfs.svg", "parser2_fcfs.pdf");
+
+print $schedule_backfilling->cmax . " " . $schedule_FCFS->cmax . "\n";
 exit;
 
