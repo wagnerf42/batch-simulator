b323d3e7a4234ad7856a078d4b7122896bf163c7
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2014-07-16 06:32:29 -0300

    Made some changes in the R file

--- backfilling_FCFS.R
+++ backfilling_FCFS.R
@@ -1,4 +1,5 @@
-data<-read.table("backfilling_FCFS.csv")
+filename<-commandArgs(TRUE)[1])
+data<-read.table(filename)
 pdf("backfilling_FCFS.pdf")
 hist(data$V1/data$V2, main="Histogram", xlab="Value of the ratio", col="gray")
 
