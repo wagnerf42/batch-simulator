3cb02e0177b88f9599fb9eb534a124f231dac95a
frederic wagner <frederic.wagner@imag.fr>
2014-10-13 18:05:42 +0200

    new test

--- test_range.pl
+++ test_range.pl
@@ -17,3 +17,10 @@ print "contains : ".join(' ', $r->processors_ids())."\n";
 
 $r->reduce_to_first(5);
 print("after reducing to 5 processors : $r\n");
+
+$r = new ProcessorRange([1,2,3,5,6,9,13,14,15,17,18]);
+$r2 = new ProcessorRange([2,3,4,5,6,7,14,15,16,17,18]);
+print "$r\n$r2\n";
+$r->remove($r2);
+print "difference : $r\n";
+
