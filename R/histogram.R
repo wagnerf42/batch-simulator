85b399197784f865e5f8e0738a7c4808adb3de31
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-01-23 10:09:58 +0100

    Removed the R files from the repository, no need to keep them here

--- R/histogram.R
+++
@@ -1,7 +0,0 @@
-filename<-commandArgs(TRUE)[1]
-output_filename<-commandArgs(TRUE)[2]
-
-data<-read.table(filename)
-pdf(output_filename)
-hist(data$V1, main=output_filename)
-
--- R/property_test.R
+++
@@ -1,6 +0,0 @@
-filename<-commandArgs(TRUE)[1]
-output_filename<-commandArgs(TRUE)[2]
-data<-read.table(filename)
-pdf(output_filename)
-plot(data$V1, data$V2, main=output_filename)
-
