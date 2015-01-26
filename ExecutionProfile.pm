7714a1e5b328545a3e1013a1e5f28babb9a083ad
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-01-26 10:44:23 +0100

    Nothing important, working on some speed comparisons

--- ExecutionProfile.pm
+++ ExecutionProfile.pm
@@ -79,7 +79,7 @@ sub remove_job {
 	my $job_ending_time = $job->submitted_ending_time();
 	my $done_until_time = $job_starting_time;
 
-	print "remove $job between $job_starting_time and $job_ending_time: $self\n";
+	#print "remove $job between $job_starting_time and $job_ending_time: $self\n";
 
 	my @new_profiles;
 	my @impacted_profiles;
@@ -135,7 +135,7 @@ sub remove_job {
 		$done_until_time = $profile_ending_time;
 	}
 
-	print "after removal: $self\n";
+	#print "after removal: $self\n";
 
 	push @new_profiles, @end_profiles;
 	$self->{profiles} = [@new_profiles];
--- fernando.pl
+++ fernando.pl
@@ -9,18 +9,15 @@ use Trace;
 use Schedule;
 use Backfilling;
 use BinarySearchTree;
-use TestPackage;
 
 my ($trace_file_name) = @ARGV;
 my $trace = Trace->new_from_swf($trace_file_name);
-$trace->reset_jobs_numbers();
+#$trace->reset_jobs_numbers();
 $trace->fix_submit_times();
 my $cpus_number = $trace->needed_cpus();
 my $cluster_size = 16;
 my $schedule = Backfilling->new(NEW_EXECUTION_PROFILE, $trace, $cpus_number, $cluster_size, BASIC);
 $schedule->run();
-
-#print join(' ', $_->job_number(), $_->{schedule_times}, $_->{improved_schedule_times}) . "\n" for @{$schedule->{jobs}};
 
 print STDERR "Done\n";
 
