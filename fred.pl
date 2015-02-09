0b173d0d529702f7e50378b0e5cadb8fa00cc860
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-02-09 14:17:15 +0100

    Latest version of the scripts used to test the new implementation

--- fred.pl
+++ fred.pl
@@ -7,17 +7,17 @@ use Data::Dumper qw(Dumper);
 use Trace;
 use Backfilling;
 
-my ($trace_file, $algorithm, $jobs_number, $cpus_number) = @ARGV;
+my ($trace_file, $jobs_number, $cpus_number) = @ARGV;
 my $cluster_size = 16;
 
 my $trace = Trace->new_from_swf($trace_file);
 $trace->remove_large_jobs($cpus_number);
 $trace->keep_first_jobs($jobs_number);
 $trace->fix_submit_times();
-#$trace->write_to_file("$jobs_number-$cpus_number.swf");
-my $schedule = Backfilling->new($algorithm, $trace, $cpus_number, $cluster_size, BASIC);
+$trace->write_to_file("$jobs_number-$cpus_number.swf");
+my $schedule = Backfilling->new($trace, $cpus_number, $cluster_size, BASIC);
 $schedule->run();
 
-$schedule->tycat("$algorithm-$cpus_number-$cluster_size.svg");
+$schedule->tycat("$cpus_number-$cluster_size.svg");
 #print "$jobs_number $cpus_number " . $schedule->{schedule_time} . "\n";
 
