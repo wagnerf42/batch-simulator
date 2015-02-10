0cc2dc42aba65973c7177c30b029e3fd32a469d5
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-02-10 11:16:51 +0100

    Moving around the BST files

--- BinarySearchTree.pm
+++ BinarySearchTree.pm
@@ -6,37 +6,4 @@ use Data::Dumper qw(Dumper);
 
 use BinarySearchTree::Node;
 
-use overload '""' => \&_stringification;
-
-sub new {
-	my ($class, $sentinel) = @_;
-
-	my $self = {
-		root => new BinarySearchTree::Node($sentinel, undef)
-	};
-
-	bless $self, $class;
-	return $self;
-}
-
-sub add {
-	my ($self, $content) = @_;
-	return $self->{root}->add($content);
-}
-
-sub find_node {
-	my ($self, $key) = @_;
-	return $self->{root}->find_node($key);
-}
-
-sub _stringification {
-	my ($self) = @_;
-	return "$self->{root}";
-}
-
-sub remove_node {
-	my ($self, $node) = @_;
-	$self->{root}->remove_node($node);
-}
-
 1;
--- BinarySearchTree/Node.pm
+++ BinarySearchTree/Node.pm
@@ -1,25 +1,23 @@
-package BinarySearchTree::Node;
-use strict;
-use warnings;
+package Node;
 
-use Data::Dumper qw(Dumper);
+use Data::Dumper;
 use Scalar::Util qw(refaddr);
-use Carp qw(confess);
 
-use overload '""' => \&_stringification, '==' => \&_is_equal;
-
+use warnings;
+use strict;
 use constant {
 	LEFT => 0,
-	RIGHT => 1
+	RIGHT => 1,
+	NONE => 2
 };
 
-sub new {
-	my ($class, $content, $father) = @_;
 
+sub new {
+	my $class = shift;
 	my $self = {
-		content => $content,
-		children => [undef, undef],
-		father => $father
+		content => shift,
+		children => [undef, undef], #left 0, right 1
+		father => shift
 	};
 
 	bless $self, $class;
@@ -27,90 +25,165 @@ sub new {
 }
 
 sub add {
-	my ($self, $content) = @_;
-	my $current = $self;
-	my $direction = $content < $current->content() ? LEFT : RIGHT;
-	my $next = $current->{children}->[$direction];
+	my $self = shift;
+	my $content = shift;
+	my $current_node = $self;
 
-	while (defined $next) {
-		$current = $next;
-		$direction = $content < $current->content() ? LEFT : RIGHT;
-		$next = $current->{children}->[$direction];
+	my $next_direction = $current_node->get_direction_for($content);
+
+	while(defined $current_node->{children}->[$next_direction]) {
+		$current_node = $current_node->{children}->[$next_direction];
+		$next_direction = $current_node->get_direction_for($content);
 	}
 
-	$next = new BinarySearchTree::Node($content, $current);
-	$current->{children}->[$direction] = $next;
-	return $next;
+	my $new_node = Node->new($content);
+
+	$current_node->{children}->[$next_direction] = $new_node;
+	$new_node->set_father($current_node,$next_direction);
+
+	print STDERR "ajout de $content\n";
+	$self->create_dot();
+	return;
 }
 
-sub find_node {
-	my ($self, $key) = @_;
-	my $current = $self;
-
-	while (defined $current) {
-		last if $current->{content} == $key;
-		my $direction = ($key < $current->{content} ? LEFT : RIGHT);
-		$current = $current->{children}->[$direction];
-	}
-
-	return $current;
-}
-
-sub remove_node {
-	my ($self, $node) = @_;
-
-	if (not defined $node->{children}->[LEFT] and not defined $node->{children}->[RIGHT]) {
-		$node->{father}->{children}->[$node->_direction()] = undef;
-
-	} elsif (not defined $node->{children}->[LEFT]) {
-		$node->{father}->{children}->[$node->_direction()] = $node->{children}->[RIGHT];
-		$node->{children}->[RIGHT]->{father} = $node->{father};
-
-	} elsif (not defined $node->{children}->[RIGHT]) {
-		$node->{father}->{children}->[$node->_direction()] = $node->{children}->[LEFT];
-		$node->{children}->[LEFT]->{father} = $node->{father};
-
+sub direction_of_unique_child {
+	my $self = shift;
+	return NONE unless $self->children_number() == 1;
+	if (defined $self->{children}->[LEFT]) {
+		return LEFT;
 	} else {
-		my $direction = int rand(2);
-		my $last_child = $self->_last_child($node->{children}->[$direction], 1 - $direction);
-		$node->{content} = $last_child->{content};
-		$self->remove_node($last_child);
+		return RIGHT;
 	}
 }
 
-sub content {
-	my ($self, $content) = @_;
-	$self->{content} = $content if defined $content;
-	return $self->{content};
+sub remove {
+	my $self = shift;
+	my $father = $self->{father};
+
+	my $unique_child_direction = $self->direction_of_unique_child();
+	if ($unique_child_direction == NONE) {
+		if ($self->children_number() == 0) {
+			# no children ! very easy case
+			$father->{children}->[get_node_direction($father, $self)] = undef;
+		} else {
+			#complex case : 2 children : exchange and remove
+			my $direction = int rand(2);
+			my $last_child = $self->{children}->[$direction]->last_child(1 - $direction);
+			$self->{content} = $last_child->{content};
+			$last_child->remove();
+		}
+	} else {
+		#easy case : we have only one child
+		$father->{children}->[get_node_direction($father, $self)] = $self->{children}->[$unique_child_direction];
+		$self->{children}->[$unique_child_direction]->{father} = $father;
+	}
+	return;
 }
 
-sub _stringification {
-	my ($self) = @_;
-	my @children_strings = map {if (defined $_) {"$_"} else {""}} @{$self->{children}};
-	my $string = join(',', @children_strings);
+# Return the direction of the node given
+sub get_node_direction {
+	my $self = shift;
+	my $child = shift;
 
-	return $self->{content} . "[$string]";
+	for my $direction (LEFT,RIGHT) {
+		next unless defined $self->{children}->[$direction];
+		return $direction if $self->{children}->[$direction] == $child;
+	}
 }
 
-sub _last_child {
-	my ($self, $node, $direction) = @_;
+sub children_number {
+	my $self = shift;
+	return scalar grep { defined $_ } @{$self->{children}};
+}
+
+# Set father of the node given
+sub set_father {
+	my $self = shift;
+	my $father = shift;
+	my $direction = shift;
+	$self->{father} = $father;
+	$father->{children}->[$direction] = $self;
+}
+
+# Return the last children of the node given
+sub last_child {
+	my $node = shift;
+	my $direction = shift;
 	while (defined $node->{children}->[$direction]) {
 		$node = $node->{children}->[$direction];
 	}
 	return $node;
 }
 
-sub _direction {
-	my ($self) = @_;
-	my $children = $self->{father}->{children};
+# Return the node of the content if he exist
+sub find_node {
+	my $self = shift;
+	my $content = shift;
+	my $current_node = $self;
 
-	return LEFT if (defined $children->[LEFT] and $self == $children->[LEFT]);
-	return RIGHT;
+	while(defined $current_node)
+	{
+		last if $current_node->{content} == $content;
+		my $direction = $current_node->get_direction_for($content);
+		$current_node = $current_node->{children}->[$direction];
+	}
+
+	return $current_node;
 }
 
-sub _is_equal {
-	my ($self, $other) = @_;
-	return refaddr($self) == refaddr($other);
+sub children {
+	my $self = shift;
+	my @children;
+	for my $direction(LEFT, RIGHT) {
+		next unless defined $self->{children}->[$direction];
+		push @children, $self->{children}->[$direction];
+	}
+	return @children;
+}
+
+# Return the direction for a content
+sub get_direction_for {
+	my $self = shift;
+	my $content = shift;
+	return ($content < $self->{content})?LEFT:RIGHT;
+}
+
+# Write information of the tree on a file
+sub dot_all_content {
+	my $self = shift;
+	my $fd = shift;
+
+	my $addr = refaddr $self;
+	my $content = $self->{content};
+
+	print $fd "$addr [label = $content];\n";
+
+	if (defined $self->{father}) {
+		my $addrf = refaddr $self->{father};
+		print $fd "$addrf -> $addr\n";
+	}
+	$_->dot_all_content($fd) for ($self->children());
+	return;
+}
+
+# Write a file .dot, create the jpg of this and display it
+my $directory = '/tmp';
+# Incremental id for graphics
+my $id_file = 0;
+sub create_dot {
+	my $self = shift;
+
+	open(my $fd, ">", "$directory/graph$id_file.dot")
+		or die "can't open > graph$id_file.dot";
+
+	print $fd "digraph G {\n";
+	$self->dot_all_content($fd);
+	print $fd "}";
+
+	system "dot -Tjpg -o$directory/graph$id_file.jpg $directory/graph$id_file.dot";
+	system "tycat $directory/graph$id_file.jpg";
+	$id_file++;
+	return;
 }
 
 1;
