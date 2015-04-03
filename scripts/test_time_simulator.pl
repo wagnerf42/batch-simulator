a327d97b7419801fbf8684d70a538d15c57b9599
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-04-03 18:09:51 +0200

    First version of the script used to test the external simulator
    This version was meant to use threads to test the execution with
    external simulator several times. Right now it is only safe to use it
    with one thread per time, since batsim will always use the same socket.
    Eventually when it is able to use different sockets we will be able to
    test it more times simultaneously.

---
+++ scripts/test_time_simulator.pl
@@ -0,0 +1,118 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Data::Dumper qw(Dumper);
+use threads;
+use threads::shared;
+use Thread::Queue;
+use Log::Log4perl qw(get_logger);
+
+use Trace;
+use Backfilling;
+
+my $trace_file = '../swf/CEA-Curie-2011-2.1-cln-b1-clean2.swf';
+my @jobs_numbers = (300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500);
+my @cpus_numbers = (128);
+my $cluster_size = 16;
+my $threads_number = 2;
+my $backfilling_variant = BASIC;
+my $results_file_name = $ARGV[0];
+
+$SIG{INT} = \&catch_signal;
+
+my $results = [];
+share($results);
+
+Log::Log4perl::init('log4perl.conf');
+my $logger = get_logger('test_time');
+$logger->info("Creating queue\n");
+
+my $q = Thread::Queue->new();
+for my $jobs_number_index (0..$#jobs_numbers) {
+	for my $cpus_number_index (0..$#cpus_numbers) {
+		$q->enqueue([$jobs_number_index, $cpus_number_index]);
+	}
+}
+$q->end();
+
+$logger->info("Creating threads");
+my @threads = map {threads->create(\&run, $_)} (0..($threads_number - 1));
+
+$logger->info("Waiting for threads to finish");
+while ((my $running_threads = threads->list()) > 0) {
+	my @joinable_threads = threads->list(threads::joinable);
+	$_->join() for (@joinable_threads);
+	sleep(5);
+}
+
+$logger->info("Writing results to file $results_file_name");
+write_results_to_file();
+
+$logger->info("Done");
+
+sub run {
+	my $id = shift;
+	my $logger = get_logger('test_time::run');
+
+	# Exit the thread if a signal is received
+	$SIG{INT} = sub { $logger->info("Killing thread $id"); threads->exit(); };
+
+	while (defined(my $instance = $q->dequeue_nb())) {
+		my ($jobs_number_index, $cpus_number_index) = @$instance;
+		my ($jobs_number, $cpus_number) = ($jobs_numbers[$jobs_number_index], $cpus_numbers[$cpus_number_index]);
+
+		$logger->info("Thread $id running ($jobs_number, $cpus_number)");
+
+		# Running schedule and batsim
+		my $schedule_thread = threads->create(\&run_schedule, $jobs_number, $cpus_number);
+		sleep(1);
+		my $batsim_result = `/home/fernando/Documents/batsim/build/batsim $platform_file $json_file`;
+
+		# Writing result
+		$results->[$jobs_number_index * @cpus_numbers + $cpus_number_index] = $schedule_thread->join();
+	}
+
+	$logger->info("Thread $id finished");
+	return;
+}
+
+sub run_schedule {
+	my $jobs_number = shift;
+	my $cpus_number = shift;
+
+	my $trace = Trace->new_from_swf($trace_file);
+	$trace->remove_large_jobs($cpus_number);
+	$trace->keep_first_jobs($jobs_number);
+	$trace->fix_submit_times();
+	$trace->reset_jobs_numbers();
+
+	my $schedule = Backfilling->new($trace, $cpus_number, $cluster_size, $backfilling_variant);
+	$schedule->run();
+
+	return $schedule->{schedule_time};
+}
+
+sub write_results_to_file {
+	open (my $file, '>', $results_file_name) or die "unable to open $results_file_name";
+
+	for my $jobs_number_index (0..$#jobs_numbers) {
+		for my $cpus_number_index (0..$#cpus_numbers) {
+			my $temp = $results->[$jobs_number_index * @cpus_numbers + $cpus_number_index];
+			print $file "$jobs_numbers[$jobs_number_index] $cpus_numbers[$cpus_number_index] " . (defined $temp ? $temp : '?') . "\n";
+		}
+		print $file "\n";
+	}
+
+	close($file);
+	return;
+}
+
+sub catch_signal {
+	my $signame = shift;
+	print STDERR "Received SIG$signame signal\n";
+	$_->kill('INT')->detach() for @threads;
+	return;
+}
+
+
