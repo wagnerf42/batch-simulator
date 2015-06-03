d72cb4f6327cf13e34dd4d04f7eaa56054de7307
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-06-03 14:18:43 +0200

    Changed the script to do a bigger test with 256 CPUs
    This commit includes the script that tests both solutions, the exact one
    and the non exact one.

--- scripts/test_platform.pl
+++ scripts/test_platform.pl
@@ -12,11 +12,11 @@ Log::Log4perl::init('log4perl.conf');
 
 my $logger = get_logger('test');
 
-my @levels = (1, 8, 16, 32);
+my @levels = (1, 2, 16, 256);
 my @available_cpus = (0..($levels[$#levels] - 1));
 
-my $removed_cpus_number = 12;
-my $required_cpus = 4;
+my $removed_cpus_number = 100;
+my $required_cpus = 16;
 
 for my $i (0..($removed_cpus_number - 1)) {
 	my $position = int(rand($levels[$#levels] - 1 - $i));
@@ -30,13 +30,12 @@ $platform->build_structure2();
 #print Dumper($platform->{root});
 my @selected_cpus = $platform->choose_cpus2($required_cpus);
 $logger->info("cpus: @selected_cpus");
-die 'stop';
 
-for my $norm (1, 2, 3, 4, 5, 6) {
-	$platform = Platform->new(\@levels, \@available_cpus, $norm);
-	$platform->build_structure();
-	#print Dumper($platform->{root});
-	my @selected_cpus = $platform->choose_cpus($required_cpus);
-	$logger->info("norm: $norm, cpus: @selected_cpus");
-}
+#for my $norm (1, 2, 3, 4, 5, 6) {
+#	$platform = Platform->new(\@levels, \@available_cpus, $norm);
+#	$platform->build_structure();
+#	#print Dumper($platform->{root});
+#	my @selected_cpus = $platform->choose_cpus($required_cpus);
+#	$logger->info("norm: $norm, cpus: @selected_cpus");
+#}
 
