1c09f73dcbb3439ee77dc8b9210c42ea4d139744
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-06-11 12:39:03 +0200

    Next attempt to generate the platform

--- scripts/generate_platform.pl
+++ scripts/generate_platform.pl
@@ -22,15 +22,29 @@ $xml->{platform}{AS} = {
 # Clusters
 for my $cluster (0..($platform_parts[$#platform_parts] - 1)) {
 	$xml->{platform}{AS}{cluster}[$cluster] = {
-		id => "AS_Root_c" . $cluster,
+		id => "C-$cluster",
 		prefix => "",
 		suffix => "",
 		radical => ($cluster * 16) . '-' . (($cluster + 1) * 16 - 1),
 		power => "286.087kf",
 		bw => "125MBps",
 		lat => "24us",
-		router_id => "AS_Root_c" . $cluster . "_r1",
+		router_id => "R-$#platform_parts-$cluster",
 	};
+
+	$xml->{platform}{AS}{link}[$cluster] = {
+		id => "L-$cluster",
+		bandwidth => "1.25GBps",
+		latency => "24us",
+	};
+
+	$xml->{platform}{AS}{ASroute}[$cluster] = {
+		src => "L-$cluster",
+		dst => "AS_Tree",
+		gw_src => "R-$#platform_parts-$cluster",
+		gw_dst => 'R-',
+		link_ctn => {id => "L-$cluster"},
+	}
 }
 
 # Tree system
@@ -38,6 +52,36 @@ $xml->{platform}{AS}{AS} = {
 	id => "AS_Tree",
 	routing => "Floyd",
 };
+
+# Build levels
+for my $level (0..($#platform_parts - 1)) {
+	my $links_number = $platform_parts[$level + 1]/$platform_parts[$level];
+	my $nodes_number = $platform_parts[$level];
+
+	for my $node_number (0..($nodes_number - 1)) {
+		push @{$xml->{platform}{AS}{AS}{router}}, {id => "R-$level-$node_number"};
+
+		for my $base_link_number (0..($links_number - 1)) {
+			my $link_number = $node_number * $links_number + $base_link_number;
+			push @{$xml->{platform}{AS}{AS}{link}}, {id => "L-$level-$link_number" , bandwidth => "1.25GBps", latency => "24us"};
+
+			push @{$xml->{platform}{AS}{AS}{route}}, {
+				src => "R-$level-$node_number",
+				dst => 'R-' . ($level + 1) . "-$link_number",
+				link_ctk => {id => "L-$level-$link_number"},
+			};
+		}
+	}
+}
+
+$xml->save('test.xml');
+open(my $file, 'test.xml');
+while (my $row = <$file>) {
+	chomp $row;
+	print "$row\n";
+}
+
+die;
 
 # Switches
 my $switches_number = sum @platform_parts[0..($#platform_parts - 1)];
@@ -67,10 +111,22 @@ for my $level (0..($#platform_parts - 1)) {
 	last;
 }
 
-$xml->save('test.xml');
-open(my $file, 'test.xml');
-while (my $row = <$file>) {
-	chomp $row;
-	print "$row\n";
+sub _build_level {
+	my $level = shift;
+	my $node = shift;
+
+	return if ($level == $#platform_parts);
+
+	my $children_number = $platform_parts[$level + 1]/$platform_parts[$level];
+	print STDERR "$level-$node-$children_number\n";
+
+	# Create the switch as a router
+	push @{$xml->{platform}{AS}{AS}{router}}, {id => "S-$level-$node"};
+
+	for my $node (0..($children_number - 1)) {
+		# Create the link
+		push @{$xml->{platform}{AS}{AS}{link}}, {id => "L-$level-$node", bandwidth => "1GBps", latency => "1ms"};
+		_build_level($level + 1, $node);
+	}
 }
 
