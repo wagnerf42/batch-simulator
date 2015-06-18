d88585f96afdb9a5e1cb97dcd2d50dc3a4751d8b
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-06-18 11:29:58 +0200

    Still working on the script

--- scripts/compare_platform.pl
+++ scripts/compare_platform.pl
@@ -13,6 +13,10 @@ Log::Log4perl::init('log4perl.conf');
 
 my $logger = get_logger('test');
 
+my @test = (1, 4, 3, 5, 4, 2);
+write_host_file(\@test, 3);
+die;
+
 my @levels = (1, 2, 4, 32);
 my @available_cpus = (0..($levels[$#levels] - 1));
 
@@ -28,20 +32,27 @@ $logger->debug("available cpus: @available_cpus");
 
 my $platform = Platform->new(\@levels, \@available_cpus, 1);
 $platform->build_structure();
-my @combinations = $platform->generate_all_combinations($required_cpus);
-
-print Dumper(@combinations);
-die;
-
 $platform->build_platform_xml();
 $platform->save_platform_xml('/tmp/platform.xml');
 
-for my $combination (@combinations) {
-	my $iterator = Algorithm::Permute->new($combination);
+my $iterator = Algorithm::Permute->new(\@available_cpus);
 
-	while (my @permutation = $iterator->next()) {
-		print STDERR "permutation @permutation\n";
-	}
+my $permutation_number = 0;
+
+while (my @permutation = $iterator->next()) {
+	$logger->debug("permutation @permutation");
+	write_host_file(\@permutation, $permutation_number);
+	$permutation_number++;
+}
+
+sub write_host_file {
+	my $permutation = shift;
+
+	my $permutation_file_name = "/tmp/permutation-$permutation_number";
+	open(my $file, '>', $permutation_file_name);
+
+	print $file "$_\n" for (@{$permutation});
+	return;
 }
 
 
