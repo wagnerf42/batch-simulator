2dbe668bc31005054cb5dcfbec4c5e9904106d1f
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-07-02 11:07:01 +0200

    Attempt to make a good script to generate the unique combinations
    I am now close to the solution, but I'm missing one part and the answer
    is not correct yet.

--- scripts/generate_unique_combinations.pl
+++ scripts/generate_unique_combinations.pl
@@ -12,7 +12,7 @@ use Platform;
 Log::Log4perl::init('log4perl.conf');
 my $logger = get_logger('test');
 
-my @levels = (1, 2, 4, 8, 64);
+my @levels = (1,3,6,12);
 my @available_cpus = (0..($levels[$#levels] - 1));
 #my @available_cpus = (0, 1, 2, 4, 5);
 my $required_cpus = 8;
@@ -24,66 +24,71 @@ $logger->info("platform: @levels");
 $logger->info("available cpus: @available_cpus");
 $logger->info("required cpus: $required_cpus");
 
-generate_cluster_combinations('6-1-1', 0);
-
-my @combinations = generate_unique_combinations(8, 0, $levels[-1]/$levels[-2]);
+my @combinations = generate_unique_combinations(2, 0);
+print Dumper(@combinations);
+die;
 save_combinations();
 
 sub generate_unique_combinations {
 	my $required_cpus = shift;
-	my $start_cluster = shift;
-	my $maximum_size = shift;
+	my $level = shift;
 
-	my $cluster_size = $levels[-1]/$levels[-2];
-	my $remaining_size = ($levels[-2] - $start_cluster) * $cluster_size;
-	return if ($required_cpus > $remaining_size);
+	return "$required_cpus" if ($level == $#levels - 1);
 
-	return unless $required_cpus;
-	return unless ($start_cluster < $levels[-2]);
-
+	my @next_combinations = next_combinations($required_cpus, $level + 1, 0, $required_cpus);
 	my @combinations;
 
-	for (my $cpus_number = min($required_cpus, $maximum_size); $cpus_number >= 1; $cpus_number--) {
+	for my $next_combination (@next_combinations) {
+		my @merging_combinations;
+		my @combination_parts = split('-', $next_combination);
+		for my $node_number (0..$#combination_parts) {
+			my @node_combinations = generate_unique_combinations($combination_parts[$node_number], $level + 1);
+			@merging_combinations = merge_combinations(\@merging_combinations, \@node_combinations);
+		}
+		push @combinations, @merging_combinations;
+	}
+	return @combinations;
+}
+
+sub merge_combinations {
+	my $combinations = shift;
+	my $node_combinations = shift;
+
+	my @merged_combinations;
+
+	# On the first call $combinations will be empty
+	return @$node_combinations unless (@{$combinations});
+
+	for my $combination (@$combinations) {
+		for my $node_combination (@$node_combinations) {
+			push @merged_combinations, join('-', $combination, $node_combination);
+		}
+	}
+
+	return @merged_combinations;
+}
+
+sub next_combinations {
+	my $required_cpus = shift;
+	my $level = shift;
+	my $node_number = shift;
+	my $maximum_cpus = shift;
+
+	return if ($node_number >= $levels[$level]);
+
+	my @combinations;
+	my $total_level_size = $levels[-1]/$levels[$level];
+
+	for (my $cpus_number = min($total_level_size, $maximum_cpus); $cpus_number >= 1; $cpus_number--) {
 		if ($required_cpus - $cpus_number) {
-			my @next_combinations = generate_unique_combinations($required_cpus - $cpus_number, $start_cluster + 1, min($cpus_number, $maximum_size));
+			my @next_combinations = next_combinations($required_cpus - $cpus_number, $level, $node_number + 1, $cpus_number);
 			push @combinations, join('-', $cpus_number, $_) for (@next_combinations);
+
 		} else {
 			push @combinations, "$cpus_number";
 		}
 	}
-
 	return @combinations;
-}
-
-sub generate_cluster_combinations {
-	my $combination = shift;
-	my $current_level = shift;
-
-	my $next_level_arity = $levels[$current_level + 1];
-	my $next_level_size = level_total_size($current_level + 1);
-
-	my @possible_divisions = possible_divisions($combination, $next_level_arity, $next_level_size);
-
-
-}
-
-sub level_total_size {
-	my $level = shift;
-	return $levels[-1]/$levels[$level];
-}
-
-sub possible_divisions {
-	my $combination = shift;
-	my $next_level_arity = shift;
-	my $next_level_size = shift;
-
-	my @combination_parts = split('-', $combination);
-
-	my @possible_divisions;
-
-	for my $i (0..
-
-
 }
 
 sub save_combinations {
@@ -92,8 +97,8 @@ sub save_combinations {
 	for my $combination (@combinations) {
 		my @combination_parts = split('-', $combination);
 		my @selected_cpus;
-		for my $cluster_number (0..$#combination_parts) {
-			my @cluster_cpus = map {$cluster_number * $cluster_size + $_} (0..($combination_parts[$cluster_number] - 1));
+		for my $node_number (0..$#combination_parts) {
+			my @cluster_cpus = map {$node_number * $cluster_size + $_} (0..($combination_parts[$node_number] - 1));
 			push @selected_cpus, @cluster_cpus;
 		}
 
