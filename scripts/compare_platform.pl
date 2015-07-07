3fa5db480977d47c4dedf58ccc87fff3ee90e044
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-07-07 14:07:15 +0200

    Version used to run the simulations on MVAPICH
    The number of the experiment is 19.

--- scripts/compare_platform.pl
+++ scripts/compare_platform.pl
@@ -13,13 +13,15 @@ use Platform;
 Log::Log4perl::init('log4perl.conf');
 my $logger = get_logger('test');
 
-my $execution_id = 15;
+my $execution_id = 19;
 my $required_cpus = 4;
 my $threads_number = 6;
-my $benchmark_class = 'B';
-my @included_benchmarks = ('cg', 'ft', 'lu');
-my @benchmarks = map {"benchmarks/$_.$benchmark_class.$required_cpus"} (@included_benchmarks);
-my @benchmarks_strings = map {"$_.$benchmark_class.$required_cpus"} (@included_benchmarks);
+#my $benchmark_class = 'B';
+#my @included_benchmarks = ('cg', 'ft', 'lu', 'ep', 'mg');
+#my @benchmarks = map {"benchmarks/$_.$benchmark_class.$required_cpus"} (@included_benchmarks);
+#my @benchmarks_strings = map {"$_.$benchmark_class.$required_cpus"} (@included_benchmarks);
+my $benchmarks_path = 'collective';
+my @benchmarks =  ('osu_allreduce', 'osu_alltoallv', 'osu_scatter', 'osu_allgather', 'osu_gather', 'osu_reduce_scatter', 'osu_allgatherv', 'osu_barrier', 'osu_reduce', 'osu_bcast', 'osu_alltoall');
 my $base_path = "experiment/combinations/combinations-$execution_id";
 my $platform_file = "$base_path/platform.xml";
 my $permutations_file = "$base_path/permutations";
@@ -70,7 +72,7 @@ sub run_instance {
 		$logger->info("thread $id runing $instance");
 
 		for my $benchmark_number (0..$#benchmarks) {
-			my $result = `./smpireplay.sh $required_cpus $platform_file $hosts_file_name $benchmarks[$benchmark_number]`;
+			my $result = `./smpireplay.sh $required_cpus $platform_file $hosts_file_name $benchmarks_path/$benchmarks[$benchmark_number]`;
 			my ($simulation_time) = ($result =~ /Simulation time (\d*\.\d*)/);
 			$results_instance->[$benchmark_number] = $simulation_time;
 		}
@@ -98,7 +100,7 @@ sub get_log_file {
 
 sub write_results {
 	open(my $file, '>', $results_file);
-	print $file "PERMUTATION " . join(' ' , @benchmarks_strings) . "\n";
+	print $file "PERMUTATION " . join(' ' , @benchmarks) . "\n";
 
 	for my $permutation_number (0..$#permutations) {
 		my @results_temp = @{$results->[$permutation_number]}[0..$#benchmarks];
