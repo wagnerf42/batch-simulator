8e203bbf92b72ba53161336fa954d431d18c9be0
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-08-13 13:24:37 +0200

    New version of the generate_permutations script

--- scripts/generate_permutations.pl
+++ scripts/generate_permutations.pl
@@ -12,19 +12,70 @@ use Platform;
 Log::Log4perl::init('log4perl.conf');
 my $logger = get_logger('generate_permutations');
 
-my $cluster_size = 2;
-my @permutations;
+my ($cluster_size, $permutation) = @ARGV;
+my @cpus = split('-', $permutation);
+my @all_permutations = permutations(\@cpus);
+my @duplicated_permutations;
+my @final_permutations;
 
-while (<>) {
-	chomp;
-	my @cpus = split('-', $_);
-	my $iterator = Algorithm::Permute->new(\@cpus);
-	while (my @permutation = $iterator->next()) {
-		push @permutations, \@permutation;
+for my $permutation (@all_permutations) {
+	unless (grep { $_ eq $permutation } @duplicated_permutations) {
+		my @equivalent_permutations = equivalent_permutations($permutation);
+		push @duplicated_permutations, @equivalent_permutations;
+		push @final_permutations, $permutation;
 	}
 }
 
-print Dumper(@permutations);
+print Dumper(@final_permutations);
+
+sub equivalent_permutations {
+	my $permutation = shift;
+
+	my @permutation_cpus = split('-', $permutation);
+	my $permutation_size = scalar @permutation_cpus;
+	my $clusters_number = $permutation_size/$cluster_size;
+	my @combined_permutations;
+
+	for my $cluster (0..($clusters_number - 1)) {
+		my @cluster_cpus = @permutation_cpus[($cluster * $cluster_size)..(($cluster + 1) * $cluster_size - 1)];
+		my @cluster_permutations = permutations(\@cluster_cpus);
+		if (scalar @combined_permutations) {
+			@combined_permutations = combine_permutations(\@combined_permutations, \@cluster_permutations);
+		} else {
+			@combined_permutations = @cluster_permutations;
+		}
+	}
+
+	return @combined_permutations;
+}
+
+sub permutations {
+	my $elements = shift;
+
+	my @permutations;
+	my $iterator = Algorithm::Permute->new($elements);
+
+	while (my @permutation = $iterator->next()) {
+		push @permutations, join('-', @permutation);
+	}
+
+	return @permutations;
+}
+
+sub combine_permutations {
+	my $initial_permutations = shift;
+	my $additional_permutations = shift;
+
+	my @final_permutations;
+
+	for my $initial_permutation (@$initial_permutations) {
+		for my $additional_permutation (@$additional_permutations) {
+			push @final_permutations, join('-', $initial_permutation, $additional_permutation);
+		}
+	}
+
+	return @final_permutations;
+}
 
 sub get_log_file {
 	return "log/generate_permutations.log";
