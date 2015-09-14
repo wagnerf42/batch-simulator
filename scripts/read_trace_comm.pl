db9edb23fe0f5ba87be249cfb86d9747aeb8c1c9
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-09-14 11:28:09 +0200

    Implement the read_trace_comm.pl script
    This script reads a .trace file generated from Simgrid and prints the
    sum of computations, for each of the ranks.

--- scripts/read_trace_comm.pl
+++ scripts/read_trace_comm.pl
@@ -5,19 +5,44 @@ use warnings;
 use Log::Log4perl qw(get_logger :no_extra_logdie_message);
 use Data::Dumper;
 
-my ($input_file) = @ARGV;
+my ($trace_file_name) = @ARGV;
 
-open(my $file, '<', $input_file) or die ('unable to open file');
+my @trace_file_names;
+my @rank_computations;
 
-my $compute_total = 0.0;
-
+open(my $file, '<', $trace_file_name) or die ('unable to open file');
 while (my $line = <$file>) {
-	my @line_parts = split(' ', $line);
-
-	if ($line_parts[1] eq 'compute') {
-		$compute_total += $line_parts[2]/1.0e6;
-	}
+	chomp $line;
+	push @trace_file_names, $line;
 }
 
-print "$compute_total\n";
+@rank_computations = map { read_trace_file($_) } (@trace_file_names);
+print benchmark_name($trace_file_name) . " " . join(' ', @rank_computations) . "\n";
+
+sub read_trace_file {
+	my $file_name = shift;
+
+	my $compute_total = 0.0;
+
+	open(my $file, '<', $file_name) or die ('unable to open file');
+
+	while (my $line = <$file>) {
+		my @line_parts = split(' ', $line);
+
+		if ($line_parts[1] eq 'compute') {
+			$compute_total += $line_parts[2]/1.0e6;
+		}
+	}
+
+	close($file);
+	return $compute_total;
+}
+
+sub benchmark_name {
+	my $file_name = shift;
+
+	my $base_name = `basename $file_name .trace`;
+	my @base_name_parts = split('-', $base_name);
+	return $base_name_parts[0];
+}
 
