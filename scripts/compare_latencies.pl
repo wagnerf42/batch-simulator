e9525fe39b6f7064741e67b98d14c641435eb1e3
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-09-25 11:23:07 +0200

    Update the compare_latencies.pl script"

--- scripts/compare_latencies.pl
+++ scripts/compare_latencies.pl
@@ -12,30 +12,16 @@ my ($execution_id, $required_cpus) = @ARGV;
 
 my $threads_number = 6;
 my @benchmarks;
-my @latencies = (1..40);
+my @latencies = (1..50);
+my @permutations;
 
-my $nasa_benchmark_class = 'B';
-my $nasa_benchmarks_path = 'benchmarks';
-my @nasa_included_benchmarks = ('cg', 'ft', 'lu');
-push @benchmarks, map {"$nasa_benchmarks_path/$_.$nasa_benchmark_class.$required_cpus"} (@nasa_included_benchmarks);
-
-my $new_benchmarks_path = 'new_benchmarks';
-my @new_included_benchmarks = ('pairs', 'neighbour', 'circular');
-#push @benchmarks, map {"$new_benchmarks_path/$_"} (@new_included_benchmarks);
-
-my $collective_benchmarks_path = 'collective';
-my @collective_included_benchmarks =  ('osu_allreduce', 'osu_alltoallv', 'osu_scatter', 'osu_allgather', 'osu_gather', 'osu_reduce_scatter', 'osu_allgatherv', 'osu_barrier', 'osu_reduce', 'osu_bcast', 'osu_alltoall');
-#push @benchmarks, map {"$collective_benchmarks_path/$_"} (@collective_included_benchmarks);
-
-my $mpi_benchmarks_path = 'mpi-benchmarks';
-my @mpi_benchmarks_included_benchmarks = ('osu_bw');
-#push @benchmarks, map {"$mpi_benchmarks_path/$_"} (@mpi_benchmarks_included_benchmarks);
-
+my $benchmark = "benchmarks/cg.B.$required_cpus";
 my $base_path = "experiment/combinations/combinations-$execution_id";
 my $platform_file = "$base_path/platform.xml";
 my $tmp_platform_file = '/tmp/platform.xml';
 my $permutations_file = "$base_path/permutations";
 my $hosts_file = '/tmp/hosts';
+my $results_file = "$base_path/compare_latencies-$execution_id.csv";
 
 $logger->info("running execution id $execution_id, $required_cpus required cpus");
 $logger->info("running benchmarks: @benchmarks");
@@ -49,38 +35,40 @@ open(my $permutations_fd, '<', $permutations_file) or $logger->logdie("permutati
 
 while (defined(my $permutation = <$permutations_fd>)) {
 	chomp($permutation);
-	my @permutation_parts = split('-', $permutation);
+	push @permutations, $permutation;
+}
 
-	my $results_file = "$base_path/$permutation.csv";
-	open(my $results_fd, '>', $results_file) or $logger->logdie("unable to create file $results_file");
-	print $results_fd "LATENCY "  . join(' ', @benchmarks) . "\n";
+open(my $results_fd, '>', $results_file) or $logger->logdie("unable to create file $results_file");
+print $results_fd "LATENCY "  . join(' ', map { "p$_" } (0..$#permutations)) . "\n";
 
-	write_host_file(\@permutation_parts);
+for my $latency (@latencies) {
+	my @latency_results;
 
-	for my $latency (@latencies) {
-		my @latency_results;
+	write_platform_file($latency);
 
-		write_platform_file($latency);
+	for my $permutation (@permutations) {
+		$logger->info("runing latency $latency permutation $permutation");
 
-		for my $benchmark (@benchmarks) {
-			$logger->info("runing $permutation-$latency-$benchmark");
+		write_host_file($permutation);
 
-			$logger->debug("./scripts/smpi/smpireplay.sh $required_cpus $tmp_platform_file $hosts_file $benchmark");
-			my $result = `./scripts/smpi/smpireplay.sh $required_cpus $tmp_platform_file $hosts_file $benchmark`;
-			$logger->logdie("error running benchmark") unless ($result =~ /Simulation time (\d*\.\d*)/);
+		#$logger->debug("./scripts/smpi/smpireplay.sh $required_cpus $tmp_platform_file $hosts_file $benchmark");
+		my $result = `./scripts/smpi/smpireplay.sh $required_cpus $tmp_platform_file $hosts_file $benchmark`;
+		$logger->logdie("error running benchmark") unless ($result =~ /Simulation time (\d*\.\d*)/);
 
-			push @latency_results, $1;
-		}
-
-		print $results_fd, "$latency " . join(' ', @latency_results);
+		push @latency_results, $1;
 	}
+
+	print $results_fd "$latency " . join(' ', @latency_results) . "\n";
+
 }
 
 sub write_host_file {
 	my $permutation = shift;
 
+	my @permutation_parts = split('-', $permutation);
+
 	open(my $fd, '>', $hosts_file);
-	print $fd "$_\n" for (@{$permutation});
+	print $fd "$_\n" for (@permutation_parts);
 	return;
 }
 
