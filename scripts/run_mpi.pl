6f85976de75232253b196f14f40e0b56082809f2
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2015-10-22 14:23:25 +0200

    Update the run_mpi.pl script

--- scripts/run_mpi.pl
+++ scripts/run_mpi.pl
@@ -9,35 +9,36 @@ use File::Slurp;
 
 # Runs several benchmarks using MPI
 
-my ($cpus_number, $hosts_file, $permutations_file, $benchmark, $output_file) = @ARGV;
+my ($job_path, $cpus_number, $benchmarks_path) = @ARGV;
 
 my $executions_number = 3;
+my $hosts_file = "$job_path/hosts";
+my $permutations_file = "$job_path/permutations";
+my $output_file = "$job_path/run_mpi.csv";
+my @benchmarks = ('cg.C', 'ft.C', 'lu.B');
 
-# Read the list of hosts and save it
+# Read stuff
 my @hosts = read_file($hosts_file, chomp => 1);
-
-# Read the list of permutations
 my @permutations = read_file($permutations_file, chomp => 1);
 
 open(my $output_fd, '>', $output_file) or die ('unable to open output file');
 $output_fd->autoflush(1);
-print $output_fd "permutation permutation_number execution_number runtime\n";
+print $output_fd "execution_number permutation permutation_number benchmark runtime\n";
 
 for my $execution (0..($executions_number - 1)) {
 	for my $permutation_number (0..$#permutations) {
-		my $permutation = $permutations[$permutation_number];
-		save_hosts_file($permutation, "/tmp/hosts-$permutation_number");
+		for my $benchmark (@benchmarks) {
+			my $permutation = $permutations[$permutation_number];
+			my $permutation_hosts_file = "$job_path/hosts-$permutation_number";
+			save_hosts_file($permutation, $permutation_hosts_file);
 
-		my $execution_time = time();
-		system "mpirun -np $cpus_number -hostfile /tmp/hosts-$permutation_number $benchmark";
-		$execution_time = time() - $execution_time;
-
-		print $output_fd "$permutation $permutation_number $execution $execution_time\n";
+			my $execution_time = run_benchmark("mpirun --mca btl_tcp_if_include br0 -np $cpus_number -hostfile $permutation_hosts_file $benchmarks_path/$benchmark.$cpus_number");
+			print $output_fd "$execution $permutation $permutation_number $benchmark $execution_time\n";
+		}
 	}
 }
 
 close($output_fd);
-
 
 sub save_hosts_file {
 	my $permutation = shift;
@@ -46,5 +47,17 @@ sub save_hosts_file {
 	my @cpus = split('-', $permutation);
 
 	write_file($output_file, map { "$hosts[$_]\n" } (@cpus));
+}
+
+sub run_benchmark {
+	my $command = shift;
+
+	my $result = `$command`;
+
+	unless ($result =~ /Time in seconds\s*=\s*(\d*\.\d*)/) {
+		die("unable to retrieve execution time from command $command");
+	}
+
+	return $1;
 }
 
