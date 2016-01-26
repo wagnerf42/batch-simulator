26dd26b5931648319affcbe08844346b4d433996
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2016-01-26 16:29:31 +0100

    Work on the platform experiment

--- scripts/run_schedule_platform.pl
+++ scripts/run_schedule_platform.pl
@@ -8,23 +8,37 @@ use Log::Log4perl qw(get_logger);
 use Trace;
 use Backfilling;
 
-my ($trace_file, $cpus_number, $cluster_size, $variant) = @ARGV;
+my ($trace_file, $cpus_number, $jobs_number, $cluster_size, $variant, $platform_levels) = @ARGV;
 
 Log::Log4perl::init('log4perl.conf');
-my $logger = get_logger();
+my $logger = get_logger('experiment');
+
+my @platform_levels_parts = split('-', $platform_levels);
 
 my $trace = Trace->new_from_swf($trace_file);
-my $schedule = Backfilling->new($trace, $cpus_number, $cluster_size, $variant);
+$trace->keep_first_jobs($jobs_number);
+
+my $schedule = Backfilling->new($trace, $cpus_number, $cluster_size, $variant, \@platform_levels_parts);
 $schedule->run();
 
 my @results = (
+	$cpus_number,
+	$jobs_number,
+	$cluster_size,
+	$variant,
 	$schedule->cmax(),
 	$schedule->contiguous_jobs_number(),
 	$schedule->local_jobs_number(),
 	$schedule->locality_factor(),
+	$schedule->bounded_stretch(10),
 	$schedule->run_time(),
 );
 
 print STDOUT join(' ', @results) . "\n";
 #$schedule->tycat();
+
+sub get_log_file {
+	return "log/generate_platform.log";
+}
+
 
--- scripts/run_schedule_platform_instances.pl
+++ scripts/run_schedule_platform_instances.pl
@@ -15,47 +15,123 @@ my @variants = (BASIC, BEST_EFFORT_CONTIGUOUS, CONTIGUOUS, BEST_EFFORT_LOCAL, LO
 my @jobs_numbers = (10);
 my $experiment_path = 'experiment/run_schedule_platform';
 
-Log::Log4perl::init('log4perl.conf');
-my $logger = get_logger('experiment');
-
 my $results = [];
 share($results);
 
-my $basic_file_name = "run_schedule_platform-$variant-$jobs_number-$execution_id";
-my $experiment_folder ="$experiment_path/$basic_file_name";
+my $basic_file_name = "run_instances_platform-";
+my $experiment_folder = "$experiment_path/$basic_file_name";
 
-#my $cpus_number = $trace->needed_cpus();
-my @platform_levels_parts = split('-', $levels);
-my $cpus_number = $platform_levels_parts[$#platform_levels_parts];
-my $cluster_size = $platform_levels_parts[$#platform_levels_parts]/$platform_levels_parts[$#platform_levels_parts - 1];
+unless (-d $experiment_folder) {
+	mkdir $experiment_folder;
+	$logger->info("experiment folder $experiment_folder created");
+}
 
-my $schedule = Backfilling->new($trace, $cpus_number, $cluster_size, $variant, \@platform_levels_parts);
-$schedule->run();
+Log::Log4perl::init('log4perl.conf');
+my $logger = get_logger('experiment');
 
-my @results = (
-	$variant,
-	$jobs_number,
-	$schedule->cmax(),
-	$schedule->contiguous_jobs_number(),
-	$schedule->local_jobs_number(),
-	$schedule->locality_factor(),
-	$schedule->bounded_stretch(),
-	$schedule->run_time(),
-);
+$logger->info("reading trace");
+my $trace = Trace->new_from_swf($trace_file);
 
-print STDOUT join(' ', (
-	'VARIANT',
-	'JOBS_NUMBER',
-	'CMAX',
-	'CONT_JOBS',
-	'LOC_JOBS',
-	'LOC_FACTOR',
-	'BOUNDED_STRETCH',
-	'RUN_TIME',
-)) . "\n";
+$logger->info("Creating queue\n");
+my $q = Thread::Queue->new();
+$q->enqueue($_) for (0..($instances - 1));
+$q->end();
 
-print STDOUT join(' ', @results) . "\n";
-#$schedule->tycat();
+$logger->info("Creating threads");
+my @threads = map {threads->create(\&run_instance, $_)} (0..($threads_number - 1));
+
+$logger->info("Waiting for threads to finish");
+$_->join() for (@threads);
+
+$run_time = time() - $run_time;
+
+$logger->info("Writing results to file $experiment_folder/$basic_file_name.csv");
+write_results_to_file();
+
+$logger->info("Done");
+
+sub run_instance {
+	my $id = shift;
+
+	my $logger = get_logger('test_time::run_instance');
+
+	while (defined(my $instance = $q->dequeue_nb())) {
+		$logger->info("Thread $id running $instance");
+
+		my $trace_instance_file = "$experiment_path/swf/$instance.swf";
+
+		my $trace_id = $database->add_trace(undef, {
+				execution => $execution_id,
+				generation_method => "random jobs",
+			});
+
+		my $results_instance = [];
+		share($results_instance);
+
+		for my $backfilling_variant (@backfilling_variants) {
+			my $schedule_thread = threads->create(\&run_schedule, $trace_instance_file, $backfilling_variant);
+			my $schedule_result = $schedule_thread->join();
+
+			push @{$results_instance}, (
+				$schedule_result->{cmax},
+				$schedule_result->{contiguous_jobs},
+				$schedule_result->{local_jobs},
+				$schedule_result->{locality_factor},
+				$schedule_result->{run_time},
+			);
+
+			push @{$results_instance}, $instance;
+			$results->[$instance] = $results_instance;
+	}
+
+	$logger->info("Thread $id finished");
+	return;
+}
+
+sub run_schedule {
+	my $trace_file = shift;
+	my $backfilling_variant = shift;
+
+
+	my $schedule_result = `$schedule_script $trace_file $cpus_number $cluster_size $backfilling_variant`;
+	my ($cmax, $contiguous_jobs_number, $local_jobs_number, $locality_factor, $run_time) = split(' ', $schedule_result);
+
+	my %result = (
+		cmax => $cmax,
+		contiguous_jobs => $contiguous_jobs_number,
+		local_jobs => $local_jobs_number,
+		locality_factor => $locality_factor,
+		run_time => $run_time,
+	);
+
+	return \%result;
+}
+
+sub write_results_to_file {
+	open (my $file, '>', "$experiment_folder/$basic_file_name.csv") or die "unable to open $experiment_folder/$basic_file_name";
+
+	my @headers;
+	for my $backfilling_variant (@backfilling_variants) {
+		push @headers, map { $BACKFILLING_VARIANT_STRINGS[$backfilling_variant] . '_' . $_ } ("CMAX", "CJOBS", "LJOBS", "LFACTOR", "RTIME");
+	}
+
+	push @headers, "INSTANCE";
+	print $file join(' ', @headers) . "\n";
+
+	for my $results_item (@{$results}) {
+		print $file join(' ', @{$results_item}) . "\n";
+	}
+
+	close($file);
+	return;
+}
+
+sub catch_signal {
+	my $signame = shift;
+	print STDERR "Received SIG$signame signal\n";
+	$_->kill('INT')->detach() for @threads;
+	return;
+}
 
 sub get_log_file {
 	return "log/generate_platform.log";
