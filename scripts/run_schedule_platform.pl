fae03349787189d85a4b7f50a2a5cbb658641da1
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2016-01-26 11:18:53 +0100

    Work on the experiment script

--- scripts/run_schedule_platform.pl
+++ scripts/run_schedule_platform.pl
@@ -11,12 +11,12 @@ use Backfilling;
 my ($trace_file, $variant, $levels, $jobs_number) = @ARGV;
 
 Log::Log4perl::init('log4perl.conf');
-my $logger = get_logger();
+my $logger = get_logger('experiment');
 
 my $trace = Trace->new_from_swf($trace_file);
 $trace->keep_first_jobs($jobs_number);
 
-my $cpus_number = $trace->needed_cpus();
+#my $cpus_number = $trace->needed_cpus();
 my @platform_levels_parts = split('-', $levels);
 my $cpus_number = $platform_levels_parts[$#platform_levels_parts];
 my $cluster_size = $platform_levels_parts[$#platform_levels_parts]/$platform_levels_parts[$#platform_levels_parts - 1];
@@ -30,6 +30,8 @@ my $schedule = Backfilling->new($trace, $cpus_number, $cluster_size, $variant, \
 $schedule->run();
 
 my @results = (
+	$variant,
+	$jobs_number,
 	$schedule->cmax(),
 	$schedule->contiguous_jobs_number(),
 	$schedule->local_jobs_number(),
@@ -39,4 +41,8 @@ my @results = (
 
 print STDOUT join(' ', @results) . "\n";
 #$schedule->tycat();
+
+sub get_log_file {
+	return "log/generate_platform.log";
+}
 
