376fdef3c9668d43d721837e2be3e1ae8cddc1d9
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2016-02-19 10:22:02 +0100

    Fix smpireplay script

--- scripts/smpi/smpireplay.sh
+++ scripts/smpi/smpireplay.sh
@@ -7,6 +7,6 @@ BENCHMARK=$4
 smpirun -ext smpi_replay \
 	--cfg=smpi/cpu_threshold:-1 \
 	--cfg=smpi/privatize_global_variables:yes \
-	--cfg=smpi/display_timing:1 \
+	--cfg=smpi/running_power:23.492E9 \
 	-np $CPUS -platform $PLATFORMFILE -hostfile $HOSTFILE \
-	./smpi_replay $BENCHMARK-$CPUS.trace 2>&1
+	./smpi_replay $BENCHMARK-$CPUS.trace
