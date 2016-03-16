c4c18e2a13830fb4e25a71ada15ce52d06f3c9dd
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2016-03-16 14:23:46 +0100

    Fix path on the smpireplay.sh script

--- scripts/smpi/smpireplay.sh
+++ scripts/smpi/smpireplay.sh
@@ -9,4 +9,4 @@ smpirun -ext smpi_replay \
 	--cfg=smpi/privatize_global_variables:yes \
 	--cfg=smpi/running_power:23.492E9 \
 	-np $CPUS -platform $PLATFORMFILE -hostfile $HOSTFILE \
-	./smpi_replay $BENCHMARK-$CPUS.trace
+	mpi/smpi_replay $BENCHMARK-$CPUS.trace
